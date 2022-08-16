d833cc936c9fc42e438e25d05530707504b49c1f
rl627 <rl627@drexel.edu>
2022-08-16 15:16:44 -0400

    Draft analysis script: set up + visualize MPG and Weight

---
+++ code/analysis.R
@@ -0,0 +1,32 @@
+{
+  # 0. Setup -----
+  
+  ## 0.1 Dependencies -----
+  library(tidyverse)
+  library(rstudioapi)
+  
+  ## 0.2 Set working directory to current file-----
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+  
+  ## 0.3 Load data -----
+  data = read_csv("../public data/mtcars.csv")
+}
+
+
+
+{
+  # 1. Analysis ---- 
+  
+  ## 1.1 EDA
+  glimpse(data)
+  
+  ## 1.2 Visualization
+  data %>% 
+    ggplot(aes( x = mpg, y = wt)) +
+    geom_point()+
+    labs(title = "Car Miles per gallon (mpg) vs weight (hp)")
+}
+
+
+
+
